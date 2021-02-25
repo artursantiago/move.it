46921f8357cfd131f95e20ff3348fb897f99e599
Artur Santiago <artursantiago2001@hotmail.com>
2021-02-24 22:47:28 -0300

    aula 2: part 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import Head from "next/head";
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
@@ -7,12 +10,17 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
